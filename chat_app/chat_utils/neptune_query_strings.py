c13823b9b65baee95bafd24062658a7357ae622c
Shreshta Balachandar <sbalachandar@cloudera.com>
2025-02-03 02:26:29 +0000

    feat: added sample neo4j query

--- chat_app/chat_utils/neptune_query_strings.py
+++ chat_app/chat_utils/neptune_query_strings.py
@@ -41,3 +41,16 @@ GET_ORDER_ITEMS="""
 MATCH (o:order)-[r:has_item]->(p:product)
 RETURN o.order_id AS order_id, p.product_category_name as product_category_name
 """
+
+GET_CUSTOMERS_WITH_TIER="""
+MATCH (c:Customer)-[:placed]->(o)-[r:has_item]->(p:Product), (l:LifetimeRewardsVariable)
+WITH c.customer_id AS customer_id, ROUND(SUM(r.price) * 100) / 100 as purchase_amount, l
+RETURN
+customer_id,
+CASE
+    WHEN purchase_amount > l.average_purchase_amount + (2 * l.stddev_purchase_amount) THEN "Diamond"
+    WHEN purchase_amount > l.average_purchase_amount + l.stddev_purchase_amount THEN "Gold"
+    WHEN purchase_amount >= l.average_purchase_amount THEN "Silver"
+    ELSE "Member"
+END AS tier
+"""
--- chat_app/chat_utils/neptune_query_wrapper.py
+++ chat_app/chat_utils/neptune_query_wrapper.py
@@ -1,5 +1,11 @@
 import logging
-from chat_utils.neptune_query_strings import GET_DEMO_CUSTOMER_IDS, GET_TIER, GET_TIERS_FOR_ALL_SAMPLE, GET_PROMOTIONS_FOR_ALL_TIERS
+from chat_utils.neptune_query_strings import (
+    GET_DEMO_CUSTOMER_IDS,
+    GET_TIER,
+    GET_TIERS_FOR_ALL_SAMPLE,
+    GET_PROMOTIONS_FOR_ALL_TIERS,
+    GET_CUSTOMERS_WITH_TIER
+    )
 import os
 import random
 
@@ -16,8 +22,15 @@ URI = os.getenv("NEO4J_ENDPOINT")
 USERNAME = os.getenv("NEO4J_USERNAME")
 PASSWORD = os.getenv("NEO4J_PASSWORD")
 
-def run_graph_query(query: str) -> Any:
-    pass
+def run_graph_query(query: str) -> List[Any]:
+    with GraphDatabase.driver(URI, auth=(USERNAME, PASSWORD)) as driver:
+        driver.verify_connectivity()
+        drs = driver.session()
+        print("Connected to Neo4j!")
+        res = drs.run(query)
+        records = list(res)
+        drs.close()
+        return records
 
 def getSampleCustomerIds()->List[str]:
     customer_ids = []
@@ -102,20 +115,37 @@ def getPromotionsForCustomerId(customer_id: str)->Tuple[str, str]:
 
     except Exception as e:
         raise Exception(f"A Neptune error occurred in getPromotionsForCustomerId: {e}")
+    
+def getCustomersWithTiers()->Any:
+    print("Getting customers and tiers")
+    result = []
+    try:
+        res = run_graph_query(GET_CUSTOMERS_WITH_TIER)
+        for rec in res:
+            result.append((rec["customer_id"], rec["tier"]))
+            
+    except Exception as e:
+        raise Exception(f"A Neo4j error occurred in getCustomersWithTiers: {e}")
+    
+    return result
+
 
 if __name__ == "__main__":
 # Execute all APIs to test
+    cus_and_tier = getCustomersWithTiers()
+    logger.info(cus_and_tier)
+
     # customer_ids = getSampleCustomerIds()
     # logger.info(customer_ids)
 
     # tier = getTierForCustomerId(random.choice(customer_ids))
     # logger.info(tier)
 
-    tiers = getTiersForAllSampleCustomers()
-    logger.info(tiers)
+    # tiers = getTiersForAllSampleCustomers()
+    # logger.info(tiers)
 
-    promotions = getPromotionsForAllTiers()
-    logger.info(promotions)
+    # promotions = getPromotionsForAllTiers()
+    # logger.info(promotions)
 
     # promotions = getPromotionsForCustomerId(random.choice(customer_ids))
     # logger.info(promotions)
