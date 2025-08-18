2af62988bd10f18e96de6dfc02d687d21a54c34b
Yash Gulati <ygulati@cloudera.com>
2025-08-18 15:50:06 +0530

    changes for LLM chatbot lab setup

--- DataServicesLabs/ClouderaAI/LLMChatbot-GenAI/CAI_LLM-hol-user-guide/8_launch_app_final/llm_app_final.py
+++ DataServicesLabs/ClouderaAI/LLMChatbot-GenAI/CAI_LLM-hol-user-guide/8_launch_app_final/llm_app_final.py
@@ -1,7 +1,6 @@
 import os
-import cmlapi
-import sys
 import gradio as gr
+import cmlapi
 import pinecone
 from pinecone import Pinecone, ServerlessSpec
 from typing import Any, Union, Optional
@@ -14,10 +13,14 @@ import time
 from typing import Optional
 import boto3
 from botocore.config import Config
+import chromadb
+from chromadb.utils import embedding_functions
+
 from huggingface_hub import hf_hub_download
 
-
-USE_PINECONE = True # Set this to False avoid any Pinecone calls
+# Set any of these to False, if not using respective parts of the lab
+USE_PINECONE = True 
+USE_CHROMA = True 
 
 EMBEDDING_MODEL_REPO = "sentence-transformers/all-mpnet-base-v2"
 
@@ -38,6 +41,34 @@ if USE_PINECONE:
     current_collection_stats = index.describe_index_stats()
     print('Total number of embeddings in Pinecone index is {}.'.format(current_collection_stats.get('total_vector_count')))
 
+    
+if USE_CHROMA:
+    # Connect to local Chroma data
+    chroma_client = chromadb.PersistentClient(path="/home/cdsw/chroma-data")
+    
+    EMBEDDING_MODEL_REPO = "sentence-transformers/all-mpnet-base-v2"
+    EMBEDDING_MODEL_NAME = "all-mpnet-base-v2"
+    EMBEDDING_FUNCTION = embedding_functions.SentenceTransformerEmbeddingFunction(model_name=EMBEDDING_MODEL_NAME)
+
+    COLLECTION_NAME = 'cml-default'
+
+    print("initialising Chroma DB connection...")
+
+    print(f"Getting '{COLLECTION_NAME}' as object...")
+    try:
+        chroma_client.get_collection(name=COLLECTION_NAME, embedding_function=EMBEDDING_FUNCTION)
+        print("Success")
+        collection = chroma_client.get_collection(name=COLLECTION_NAME, embedding_function=EMBEDDING_FUNCTION)
+    except:
+        print("Creating new collection...")
+        collection = chroma_client.create_collection(name=COLLECTION_NAME, embedding_function=EMBEDDING_FUNCTION)
+        print("Success")
+
+    # Get latest statistics from index
+    current_collection_stats = collection.count()
+    print('Total number of embeddings in Chroma DB index is ' + str(current_collection_stats))
+    
+    
 ## TO DO GET MODEL DEPLOYMENT
 ## Need to get the below prgramatically in the future iterations
 client = cmlapi.default_client(url=os.getenv("CDSW_API_URL").replace("/api/v1", ""), cml_api_key=os.getenv("CDSW_APIV2_KEY"))
@@ -54,7 +85,9 @@ MODEL_ACCESS_KEY = selected_model.access_key
 MODEL_ENDPOINT = os.getenv("CDSW_API_URL").replace("https://", "https://modelservice.").replace("/api/v1", "/model?accessKey=")
 MODEL_ENDPOINT = MODEL_ENDPOINT + MODEL_ACCESS_KEY
 
-# Double check default region
+#MODEL_ACCESS_KEY = os.environ["CML_MODEL_KEY"]
+#MODEL_ENDPOINT = "https://modelservice.ml-8ac9c78c-674.se-sandb.a465-9q4k.cloudera.site/model?accessKey=" + MODEL_ACCESS_KEY
+
 if os.environ.get("AWS_DEFAULT_REGION") == "":
     os.environ["AWS_DEFAULT_REGION"] = "us-west-2"
 
@@ -123,12 +156,13 @@ def main():
     # Create the Gradio Interface
     demo = gr.ChatInterface(
         fn=get_responses, 
+        #examples=[["What is Cloudera?", "AWS Bedrock Claude v2.1", 0.5, "100"], ["What is Apache Spark?", 0.5, "100"], ["What is CML HoL?", 0.5, "100"]], 
         title="Enterprise Custom Knowledge Base Chatbot",
         description = DESC,
-        additional_inputs=[gr.Radio(['Local Mistral 7B', 'AWS Bedrock Claude v2.1'], label="Select Foundational Model", value="AWS Bedrock Claude v2.1", visible=False), 
+        additional_inputs=[gr.Radio(['Local Mistral 7B', 'AWS Bedrock Claude v2.1'], label="Select Foundational Model", value="AWS Bedrock Claude v2.1"), 
                            gr.Slider(minimum=0.01, maximum=1.0, step=0.01, value=0.5, label="Select Temperature (Randomness of Response)"),
                            gr.Radio(["50", "100", "250", "500", "1000"], label="Select Number of Tokens (Length of Response)", value="250"),
-                           gr.Radio(['None', 'Pinecone'], label="Vector Database Choices", value="None")],
+                           gr.Radio(['None', 'Pinecone', 'Chroma'], label="Vector Database Choices", value="None")],
         retry_btn = None,
         undo_btn = None,
         clear_btn = None,
@@ -147,13 +181,7 @@ def main():
 # Helper function for generating responses for the QA app
 def get_responses(message, history, model, temperature, token_count, vector_db):
     
-    
-    # Process chat history
-    #chat_history_string = '; '.join([strng for xchng in history for strng in xchng])
-    #print(f"Chat so far {history}")
-    
-    
-    if model == "'Local Mistral 7B":
+    if model == "Local Mistral 7B":
         
         if vector_db == "None":
             context_chunk = ""
@@ -165,6 +193,32 @@ def get_responses(message, history, model, temperature, token_count, vector_db):
                 yield response[:i+1]
                 
         elif vector_db == "Pinecone":
+            # TODO: sub this with call to Pinecone to get context chunks
+            #response = "ERROR: Pinecone is not implemented for LLama yet"
+            
+            # Vector search the index
+            context_chunk, source, score = get_nearest_chunk_from_pinecone_vectordb(index, message)
+            
+            # Call CML hosted model
+            response = get_llama2_response_with_context(message, context_chunk, temperature, token_count)
+            
+            # Add reference to specific document in the response
+            response = f"{response}\n\n For additional info see: {url_from_source(source)}"
+            
+            # Stream output to UI
+            for i in range(len(response)):
+                time.sleep(0.02)
+                yield response[:i+1]
+                
+        elif vector_db == "Chroma":
+            # Vector search in Chroma
+            context_chunk, source = get_nearest_chunk_from_chroma_vectordb(collection, message)
+            
+            # Call CML hosted model
+            response = get_llama2_response_with_context(message, context_chunk, temperature, token_count)
+            
+            # Add reference to specific document in the response
+            response = f"{response}\n\n For additional info see: {url_from_source(source)}"
             
             # Stream output to UI
             for i in range(len(response)):
@@ -174,8 +228,7 @@ def get_responses(message, history, model, temperature, token_count, vector_db):
     elif model == "AWS Bedrock Claude v2.1":
         if vector_db == "None":
             # No context call Bedrock
-            context_chunk = ""
-            response = get_bedrock_response_with_context(message, context_chunk, temperature, token_count)
+            response = get_bedrock_response_with_context(message, "", temperature, token_count)
         
             # Stream output to UI
             for i in range(len(response)):
@@ -194,6 +247,21 @@ def get_responses(message, history, model, temperature, token_count, vector_db):
             # Stream output to UI
             for i in range(len(response)):
                 time.sleep(0.01)
+                yield response[:i+1]
+                
+        elif vector_db == "Chroma":
+            # Vector search in Chroma
+            context_chunk, source = get_nearest_chunk_from_chroma_vectordb(collection, message)
+            
+            # Call CML hosted model
+            response = get_bedrock_response_with_context(message, context_chunk, temperature, token_count)
+            
+            # Add reference to specific document in the response
+            response = f"{response}\n\n For additional info see: {url_from_source(source)}"
+            
+            # Stream output to UI
+            for i in range(len(response)):
+                time.sleep(0.02)
                 yield response[:i+1]
 
 def url_from_source(source):
@@ -233,6 +301,20 @@ def load_context_chunk_from_data(id_path):
     with open(id_path, "r") as f: # Open file in read mode
         return f.read()
 
+
+# Get embeddings for a user question and query Chroma vector DB for nearest knowledge base chunk
+def get_nearest_chunk_from_chroma_vectordb(collection, question):
+    ## Query Chroma vector DB 
+    ## This query returns the two most similar results from a semantic search
+    response = collection.query(
+                    query_texts=[question],
+                    n_results=1
+                    # where={"metadata_field": "is_equal_to_this"}, # optional filter
+                    # where_document={"$contains":"search_string"}  # optional filter
+    )
+    #print(results)
+    
+    return response['documents'][0][0], response['ids'][0][0]
 
 def get_bedrock_response_with_context(question, context, temperature, token_count):
     
@@ -276,18 +358,30 @@ def get_bedrock_response_with_context(question, context, temperature, token_coun
     
 # Pass through user input to LLM model with enhanced prompt and stop tokens
 def get_llama2_response_with_context(question, context, temperature, token_count):
-
-    question = "Answer this question based on the given context. Question: " + str(question)
     
-    question_and_context = question + " Here is the context: " + str(context)
-
-    try:
-        
+    llama_sys = f"<s>[INST]You are a helpful, respectful and honest assistant. If you are unsure about an answer, truthfully say \"I don't know\"."
+    
+    if context == "":
         # Following LLama's spec for prompt engineering
-        llama_sys = f"<<SYS>>\n You are a helpful, respectful and honest assistant. If you are unsurae about an answer, truthfully say \"I don't know\".\n<</SYS>>\n\n"
-        llama_inst = f"[INST]Use your own knowledge and additionally use the following information to answer the user's question: {context} [/INST]"
-        question_and_context = f"{llama_sys} {llama_inst} [INST] User: {question} [/INST]"
+        # llama_inst = f"Please answer the user question.[/INST]</s>"
+        # question_and_context = f"{llama_sys} {llama_inst} \n [INST] {question} [/INST]"
+        question_and_context = (
+            "You are a helpful and honest assistant. "
+            "If you are unsure about an answer, say 'I don't know'. "
+            f"Use your knowledge to answer the question: {question}"
+        )
+    else:
+        # Add context to the question
+        # llama_inst = f"Anser the user's question based on the folloing information:\n {context}[/INST]</s>"
+        # question_and_context = f"{llama_sys} {llama_inst} \n[INST] {question} [/INST]"
+        question_and_context = (
+            "You are a helpful and honest assistant. "
+            "If you are unsure about an answer, say 'I don't know'. "
+            f"Use your knowledge to answer the question: {question}"
+        )
         
+    try:
+        # Build a request payload for CML hosted model
         data={ "request": {"prompt":question_and_context,"temperature":temperature,"max_new_tokens":token_count,"repetition_penalty":1.0} }
         
         r = requests.post(MODEL_ENDPOINT, data=json.dumps(data), headers={'Content-Type': 'application/json'})
@@ -296,9 +390,9 @@ def get_llama2_response_with_context(question, context, temperature, token_count
         print(f"Request: {data}")
         print(f"Response: {r.json()}")
         
-        #no_inst_response = str(r.json()['response'])[len(question_and_context)+2:]
+        no_inst_response = str(r.json()['response']['prediction']['response'])[len(question_and_context)-6:]
             
-        return "TEST" #no_inst_response
+        return no_inst_response
         
     except Exception as e:
         print(e)
